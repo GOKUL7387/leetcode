432088857cb8d873ec9d5eacb61fcf0fab982ad6
GOKUL <147173298+GOKUL7387@users.noreply.github.com>
2025-03-18 21:54:04 +0530

    Climbing Stairs.java

---
+++ Climbing Stairs.java
@@ -0,0 +1,22 @@
+link: https://leetcode.com/problems/climbing-stairs/description/
+
+code:
+
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2) 
+        {
+            return n;
+        }
+        int x = 1;
+        int y = 2;
+        int current = 0;
+        for (int i = 3; i <= n; i++) {
+            current = x+y;
+            x=y;
+            y=current;
+        }
+        return current;
+
+    }
+}
